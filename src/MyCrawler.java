96caeed14f4e5c0d45f8dd8dfc98acf3991e2b9e
naveen2016 <naveen.prasath@intelent.com>
2017-01-20 16:40:46 -0500

    crawler

--- src/MyCrawler.java
+++ src/MyCrawler.java
@@ -45,55 +45,55 @@ public class MyCrawler {
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("user-data-dir=C:\\Users\\navee\\Desktop\\JE.exe\\Default");
-//        options.addArguments("incognito");
+        options.addArguments("incognito");
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         WebDriver driver = new ChromeDriver(capabilities);
-//        driver.manage().window().setSize(new Dimension(200,200));
-//        driver.get("https://www.directenergy.com/texas");
-//        driver.findElement(By.name("goto_service_type")).sendKeys("electric_grid_url");
-//        driver.findElement(By.name("zipcode")).clear();
-//        driver.findElement(By.name("zipcode")).sendKeys("77098");
-//        driver.findElement(By.cssSelector(".startForm button em")).submit();
-//        Thread.sleep(800);
-//        Document TX_PWRH = Jsoup.parse(driver.getPageSource());
-//        String TX_PWRHtext = "Live Brighter 12 Fixed-Rate Plan for 12 Months, " +extractPrice(TX_PWRH.select("div[style=z-index:197]")
-//                .select("div[addr-title=Lock in Your Rate for 12 Months]").select("div[class=price]").text());
-//
-//        driver.get("https://www.directenergy.com/texas");
-//        driver.findElement(By.name("goto_service_type")).sendKeys("electric_grid_url");
-//        driver.findElement(By.name("zipcode")).clear();
-//        driver.findElement(By.name("zipcode")).sendKeys("75201");
-//        driver.findElement(By.cssSelector(".startForm button em")).submit();
-//        Thread.sleep(800);
-//        Document TX_PWRD = Jsoup.parse(driver.getPageSource());
-//        String TX_PWRDtext = "Live Brighter 12 Fixed-Rate Plan for 12 Months, " +extractPrice(TX_PWRD.select("div[style=z-index:197]")
-//                .select("div[addr-title=Lock in Your Rate for 12 Months]").select("div[class=price]").text());
-//
-//        driver.get("https://shop.constellation.com/#/19130/PECO_");
-//        driver.navigate().refresh();
-//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.HOURS);
-//        Document PECO = Jsoup.parse(driver.getPageSource());
-//        String PECOtext = PECO.body().text();
-//
-//        driver.get("https://shop.constellation.com/#/17815/PPL_");
-//        driver.navigate().refresh();
-//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.HOURS);
-//        Document PPL = Jsoup.parse(driver.getPageSource());
-//        String PPLtext = PPL.body().text();
-//
-//        driver.get("https://shop.constellation.com/#/10001/CONED_");
-//        driver.navigate().refresh();
-//        driver.manage().timeouts().implicitlyWait(30, TimeUnit.HOURS);
-//        Document CONED = Jsoup.parse(driver.getPageSource());
-//        String CONEDtext = CONED.body().text();
-//        driver.quit();
-//
-//        System.out.println("DirectEnergy_Alberta_PWR, " +AB_PWRtext);
-//        System.out.println("DirectEnergy_Houston_PWR, " +TX_PWRHtext);
-//        System.out.println("DirectEnergy_Dallas_PWR, " +TX_PWRDtext);
-//        System.out.println("constellation_19130_PWR, 12 Month Fixed Rate, " +extractPrice(PECOtext.substring(PECOtext.lastIndexOf("12 Month Fixed Rate")+3, PECOtext.lastIndexOf("12 Month Fixed Rate")+35)));
-//        System.out.println("constellation_17815_PWR, 12 Month Fixed Rate, " +extractPrice(PPLtext.substring(PPLtext.lastIndexOf("12 Month Fixed Rate")+3, PPLtext.lastIndexOf("12 Month Fixed Rate") +35)));
-//        System.out.println("constellation_10001_PWR, 12 Month Fixed Rate, " +extractPrice(CONEDtext.substring(CONEDtext.lastIndexOf("12 Month Fixed Rate")+3, CONEDtext.lastIndexOf("12 Month Fixed Rate") +35)));
+        driver.manage().window().setSize(new Dimension(200,200));
+        driver.get("https://www.directenergy.com/texas");
+        driver.findElement(By.name("goto_service_type")).sendKeys("electric_grid_url");
+        driver.findElement(By.name("zipcode")).clear();
+        driver.findElement(By.name("zipcode")).sendKeys("77098");
+        driver.findElement(By.cssSelector(".startForm button em")).submit();
+        Thread.sleep(800);
+        Document TX_PWRH = Jsoup.parse(driver.getPageSource());
+        String TX_PWRHtext = "Live Brighter 12 Fixed-Rate Plan for 12 Months, " +extractPrice(TX_PWRH.select("div[style=z-index:197]")
+                .select("div[addr-title=Lock in Your Rate for 12 Months]").select("div[class=price]").text());
+
+        driver.get("https://www.directenergy.com/texas");
+        driver.findElement(By.name("goto_service_type")).sendKeys("electric_grid_url");
+        driver.findElement(By.name("zipcode")).clear();
+        driver.findElement(By.name("zipcode")).sendKeys("75201");
+        driver.findElement(By.cssSelector(".startForm button em")).submit();
+        Thread.sleep(800);
+        Document TX_PWRD = Jsoup.parse(driver.getPageSource());
+        String TX_PWRDtext = "Live Brighter 12 Fixed-Rate Plan for 12 Months, " +extractPrice(TX_PWRD.select("div[style=z-index:197]")
+                .select("div[addr-title=Lock in Your Rate for 12 Months]").select("div[class=price]").text());
+
+        driver.get("https://shop.constellation.com/#/19130/PECO_");
+        driver.navigate().refresh();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.HOURS);
+        Document PECO = Jsoup.parse(driver.getPageSource());
+        String PECOtext = PECO.body().text();
+
+        driver.get("https://shop.constellation.com/#/17815/PPL_");
+        driver.navigate().refresh();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.HOURS);
+        Document PPL = Jsoup.parse(driver.getPageSource());
+        String PPLtext = PPL.body().text();
+
+        driver.get("https://shop.constellation.com/#/10001/CONED_");
+        driver.navigate().refresh();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.HOURS);
+        Document CONED = Jsoup.parse(driver.getPageSource());
+        String CONEDtext = CONED.body().text();
+        driver.quit();
+
+        System.out.println("DirectEnergy_Alberta_PWR, " +AB_PWRtext);
+        System.out.println("DirectEnergy_Houston_PWR, " +TX_PWRHtext);
+        System.out.println("DirectEnergy_Dallas_PWR, " +TX_PWRDtext);
+        System.out.println("constellation_19130_PWR, 12 Month Fixed Rate, " +extractPrice(PECOtext.substring(PECOtext.lastIndexOf("12 Month Fixed Rate")+3, PECOtext.lastIndexOf("12 Month Fixed Rate")+35)));
+        System.out.println("constellation_17815_PWR, 12 Month Fixed Rate, " +extractPrice(PPLtext.substring(PPLtext.lastIndexOf("12 Month Fixed Rate")+3, PPLtext.lastIndexOf("12 Month Fixed Rate") +35)));
+        System.out.println("constellation_10001_PWR, 12 Month Fixed Rate, " +extractPrice(CONEDtext.substring(CONEDtext.lastIndexOf("12 Month Fixed Rate")+3, CONEDtext.lastIndexOf("12 Month Fixed Rate") +35)));
         Scanner sc = new Scanner(new File("urls.txt"));
         while(sc.hasNextLine()) {
             String url = sc.nextLine();
